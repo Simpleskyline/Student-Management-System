4356a194e53e726309e622f8aaa9104d4445304c
skyline <telvintran@gmail.com>
2025-08-30 16:55:42 +0300

    added user authentication

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 from flask import Flask, render_template, redirect, url_for, request, flash
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy # provides database integration with SQLAlchemy
 from flask_login import LoginManager, login_user, login_required, logout_user, UserMixin, current_user
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret-key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sms.db'
-db = SQLAlchemy(app)
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+app.config['SECRET_KEY'] = 'secret-key' # used for session security
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sms.db' # configures the database to use SQLite, storing data in a file called sms.db
+db = SQLAlchemy(app) # initializes the database
+login_manager = LoginManager(app) # sets up user session management
+login_manager.login_view = 'login' # redirects unauthenticated users to the login page
 
 
 # ----------------------------
-# MODELS
+# DATABASE MODELS
+# inherits from UserMixin to provide user authentication features
 # ----------------------------
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -26,7 +27,8 @@ class Student(db.Model):
     age = db.Column(db.Integer, nullable=False)
     created_by = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-
+# it is required by Flask-Login to load a user object the database based on their user_id
+# it retrieves a user record by ID
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
@@ -42,6 +44,8 @@ def home():
 
 
 # REGISTER
+# GET: Displays the registration form(register.html)
+# POST: processes form data
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == "POST":
@@ -88,6 +92,8 @@ def dashboard():
 
 
 # ADD STUDENT (admin & teacher only)
+# GET: Displays the form to add a student (add_student.html).
+# POST: Creates a new Student record with the provided, saves it, and redirects to the dashboard.
 @app.route('/add-student', methods=['GET', 'POST'])
 @login_required
 def add_student():
@@ -108,6 +114,7 @@ def add_student():
 
 
 # DELETE STUDENT (admin only)
+# Allows only admins to delete a student by ID
 @app.route('/delete-student/<int:student_id>')
 @login_required
 def delete_student(student_id):
@@ -123,13 +130,15 @@ def delete_student(student_id):
 
 
 # LOGOUT
+# Logs out the current user and redirects to the login page.
 @app.route('/logout')
 @login_required
 def logout():
     logout_user()
     return redirect(url_for("login"))
 
-
+# Initializes the database when the app starts
+# runs the flask app in debug mode (for development only)
 if __name__ == "__main__":
     with app.app_context():
         db.create_all()
